59d6e1ce67d1b839048569ba8b521c4570f7f63f
THOMAS-PC\Thomas <tomepp92@gmail.com>
2018-01-31 09:52:36 -0600

    Map
    Added map controls

--- src/jsx/pages/MapComponent/MapComponent.jsx
+++ src/jsx/pages/MapComponent/MapComponent.jsx
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import L from 'leaflet';
 import Draw from 'leaflet-draw'
 import './MapComponent.css'
+import { Button } from 'react-bootstrap';
 // import ReactDOM from 'react-dom';
 
 export default class MyMap extends Component {
 
+    constructor(props){
+        super(props);
+        this.state={
+            drawable: true
+        }
+    }
     componentDidMount() {
-
-
 
         var map = L.map('map').setView([21.1523342, -101.7135019], 13);
         L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -22,11 +27,32 @@ export default class MyMap extends Component {
         map.addLayer(drawnItems);
 
         var drawControl = new L.Control.Draw({
+            draw: this.state.drawable,
+            position: 'topright',
             edit: {
-                featureGroup: drawnItems
+                featureGroup: drawnItems,
+                edit: true,
+                remove: false
             }
         });
+
         map.addControl(drawControl);
+
+        map.on(L.Draw.Event.CREATED, (e) => {
+            var type = e.layerType,
+                layer = e.layer;
+            console.log(map.layers);
+            // Do whatever else you need to. (save to db; add to map etc)
+            map.addLayer(layer);
+        });
+
+        // map.on(L.Draw.Event.EDITED, (e) => {
+        //     var type = e.layerType,
+        //         layer = e.layer;
+        //     console.log(layer);
+        //     // Do whatever else you need to. (save to db; add to map etc)
+        //     map.addLayer(layer);
+        // });
 
         // var map = L.map('map', {drawControl: true}).setView([51.505, -0.09], 13);
         // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
@@ -44,7 +70,11 @@ export default class MyMap extends Component {
     }
     render() {
         return (
-            <div id="map" className="Map"></div>
+            <div>
+                <div id="map" className="Map"></div>
+                <Button className="Button" onClick={() => this.setState({ drawable: true })}bsStyle="success" bsSize="large" type="submit" block >Guardar</Button>&nbsp;
+            </div>
+
         );
     }
 }
