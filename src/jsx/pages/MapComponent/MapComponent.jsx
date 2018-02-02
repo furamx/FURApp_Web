8b1a84ae5456787c7d690ae9a46c3666cfcd73f1
Thomas <tomepp92@gmail.com>
2018-02-02 13:54:57 -0600

    Panels
    Added Collapsible panels, added method to create zone.

--- src/jsx/pages/MapComponent/MapComponent.jsx
+++ src/jsx/pages/MapComponent/MapComponent.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import L from 'leaflet';
 import Draw from 'leaflet-draw';
 import './MapComponent.css'
-import { Button, Row, Col, FormGroup, FormControl, Panel, PanelGroup } from 'react-bootstrap';
+import { Button, Row, Col, FormGroup, FormControl, Panel, PanelGroup, ControlLabel, Glyphicon, InputGroup } from 'react-bootstrap';
 import MapList from './../MapListComponent/MapListComponent';
 
 
@@ -14,9 +14,74 @@ export default class MyMap extends Component {
             drawable: true,
             dropdownTitle: 'Eventos',
             mapName: '',
-            hidden: true
+            hidden: true,
+            areaName: '',
+            panelOpen: false,
+            value: [],
+            count: 5
         }
+
+        this.handleAreaSubmit = this.handleAreaSubmit.bind(this);
+        this.handleZonesSubmit = this.handleZonesSubmit.bind(this);
+        this.createZone = this.createZone.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.removeClick = this.removeClick.bind(this);
     }
+
+    handleAreaSubmit(e) {
+        e.preventDefault();
+        alert('area submited');
+        this.setState({ panelOpen: true });
+    }
+
+    handleZonesSubmit(e) {
+        e.preventDefault();
+        alert('zones submited');
+    }
+
+    handleChange(i, event) {
+        let value = this.state.value.slice();
+        value[i] = event.target.value;
+        this.setState({ value });
+    }
+
+    removeClick(i) {
+        let value = this.state.value.slice();
+        value.splice(i, 1);
+        this.setState({
+            count: this.state.count - 1,
+            value
+        })
+    }
+
+    createZone() {
+        let zones = [];
+        for (let i = 1; i < this.state.count; i++) {
+            zones.push(
+                <div key={i}>
+                    <FormGroup>
+                        <InputGroup>
+                            <FormControl
+                                type="text"
+                                value={this.state.value[i] || ''}
+                                onChange={this.handleChange.bind(this, i)}
+                                placeholder="Nombre de la zona" />
+                            <InputGroup.Button>
+                                <Button
+                                    block
+                                    className="dynamic-button"
+                                    onClick={this.removeClick.bind(this, i)}
+                                    bsStyle='danger'><Glyphicon glyph="glyphicon glyphicon-remove" />
+                                </Button>
+                            </InputGroup.Button>
+                        </InputGroup>
+                    </FormGroup>
+                </div>
+            );
+        }
+        return zones;
+    }
+
     componentDidMount() {
 
         var map = L.map('map').setView([21.1523342, -101.7135019], 13);
@@ -72,6 +137,7 @@ export default class MyMap extends Component {
         // });
         // map.addControl(drawControl);
     }
+
     render() {
         return (
             <div>
@@ -85,28 +151,73 @@ export default class MyMap extends Component {
                 </Row>
                 <Row className='Row'>
                     <Col xs={12} md={12} lg={12}>
-                        <PanelGroup>
-                            <Panel id="collapsible-panel-1" expanded={this.props.open}>
-                                <Panel.Collapse>
-                                    <Panel.Body>
-                                        {/* Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                        accusamus terry richardson ad squid. Nihil anim keffiyeh
-                                        helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                                        ea proident. */}
-                                        <Zones/>
-                                    </Panel.Body>
-                                </Panel.Collapse>
+                        <PanelGroup id="panel-group-1" >
+                            <Panel id="collapsible-panel-1" expanded={this.props.open} onToggle={this.props.open} eventKey="1" bsStyle="success">
+                                <Panel.Heading className="PanelHeader" hidden={!this.props.open}>
+                                    <Panel.Title>Área</Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>
+                                    <form onSubmit={this.handleAreaSubmit}>
+                                        <Row className="ControlLabel">
+                                            <Col xs={12} md={12} lg={12} >
+                                                <ControlLabel >Ingrese el nombre del área</ControlLabel>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={12} lg={11} >
+                                                <FormGroup>
+                                                    <FormControl type="text"
+                                                        value={this.state.areaName}
+                                                        onChange={(e) => this.setState({ areaName: e.target.value })}
+                                                        placeholder="Nombre del área">
+                                                    </FormControl>
+                                                </FormGroup>
+                                            </Col>
+                                            <Col xs={12} md={12} lg={1} >
+                                                <FormGroup>
+                                                    {/* {
+                                                        this.state.panelOpen ?
+                                                            <Button bsStyle="danger" block bsSize="large" type="button" onClick={() => {this.setState({ panelOpen: false })}}>
+                                                                <Glyphicon glyph="glyphicon glyphicon-floppy-remove" />
+                                                            </Button> :
+                                                            <Button bsStyle="success" block bsSize="large" type="submit">
+                                                                <Glyphicon glyph="glyphicon glyphicon-floppy-disk" />
+                                                            </Button>
+                                                    } */}
+                                                    <Button bsStyle="success" block type="submit">
+                                                        <Glyphicon glyph="glyphicon glyphicon-floppy-disk" />
+                                                    </Button>
+                                                </FormGroup>
+                                            </Col>
+                                        </Row>
+                                    </form>
+                                </Panel.Body>
                             </Panel>
-                            <Panel id="collapsible-panel-2" expanded={this.props.open}>
-                                <Panel.Collapse>
-                                    <Panel.Body>
-                                        {/* Anim pariatur cliche reprehenderit, enim eiusmod high life
-                                        accusamus terry richardson ad squid. Nihil anim keffiyeh
-                                        helvetica, craft beer labore wes anderson cred nesciunt sapiente
-                                        ea proident. */}
-                                        <Zones/>
-                                    </Panel.Body>
-                                </Panel.Collapse>
+                            <Panel id="collapsible-panel-2" expanded={this.state.panelOpen} onToggle={this.props.open} eventKey="2" bsStyle="success">
+                                <Panel.Heading className="PanelHeader" hidden={!this.props.open}>
+                                    <Panel.Title>Zonas</Panel.Title>
+                                </Panel.Heading>
+                                <Panel.Body collapsible>
+                                    <form onSubmit={this.handleZonesSubmit}>
+                                        <Row className="ControlLabel">
+                                            <Col xs={12} md={12} lg={12} >
+                                                <ControlLabel className="ControlLabel">Ingrese el nombre de las zonas</ControlLabel>
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={12} lg={12} >
+                                                {this.createZone()}
+                                            </Col>
+                                        </Row>
+                                        <Row>
+                                            <Col xs={12} md={12} lg={12} >
+                                                <Button bsStyle="success" block type="submit">
+                                                    Guardar
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </form>
+                                </Panel.Body>
                             </Panel>
                         </PanelGroup>
                     </Col>
@@ -143,10 +254,4 @@ export default class MyMap extends Component {
             </div>
         );
     }
-}
-
-const Zones = () => {
-    return (
-        <div>Hello there</div>
-    );
 }
